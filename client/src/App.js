485ddd621e5d1603ba9c512aee9ecb2ed673beff
HelloAyan <18103373@iubat.edu>
2022-07-15 19:48:27 +0600

    Modified code

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Edit src/App.js and save to reload.
+        </h1>
       </header>
     </div>
   );
