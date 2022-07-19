566b141e4cd4823a8bd6d6fef7f4766239f5748b
HelloAyan <18103373@iubat.edu>
2022-07-19 18:38:20 +0600

    route by middleware

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const booksRoute = require();
 
 const app = express();
-
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-    res.send("Hello World");
-})
+app.use('/books', booksRoute)
 
 app.listen(5000, ()=>{
     console.log("App listening on port 5000");
