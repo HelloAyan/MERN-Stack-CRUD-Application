115a833fb2710ddd9ba68a33c19ebcd7cf679474
HelloAyan <18103373@iubat.edu>
2022-07-21 23:57:42 +0600

    Completed routes

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,19 +3,51 @@ const router = express.Router();
 const book = require('../models/book');
 
 router.get('/', (req, res)=>{
-    res.json({data: "Hello World"});
+    book.find()
+    .then((data) =>{
+        res.json(data);
+    })
+    .catch((err)=>{
+        res.json({message: err});
+    })
 })
 
 router.post('/', (req, res)=>{
-    res.json({data: "Hello World POST"});
+    const books = new book({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    books.save()
+    .then((data) =>{
+        res.json(data);
+    })
+    .catch((err) =>{
+        res.json({message: err});
+    })
 })
 
 router.delete('/:id', (req, res)=>{
-    res.json({data: "Hello World DELETE"});
+    book.deleteOne({_id: req.params.id})
+    .then((data) =>{
+        res.json(data);
+    })
+    .catch((err) =>{
+        res.json({message: err})
+    })
 })
 
 router.patch('/:id', (req, res)=>{
-    res.json({data: "Hello World PATCH"});
+    book.updateOne({_id: req.params.id},
+        {
+            $set: {description: req.body.description}
+        })
+    .then((data) =>{
+        res.json(data);
+    })
+    .catch((err) =>{
+        res.json({message: err});
+    })
 })
 
 module.exports = router;
