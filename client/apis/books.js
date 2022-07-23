03379bdce262b55fa789de9c8c64b86979e01a82
HelloAyan <18103373@iubat.edu>
2022-07-23 23:47:01 +0600

    Added addbookAPI function

--- client/apis/books.js
+++ client/apis/books.js
@@ -1,5 +1,17 @@
 export function getBooksAPI(){
     return fetch('http://localhost:5000/books')
-    .then(res => res)
+    .then(res => res.json)
+    .then(data => data);
+}
+
+export function addBookAPI(book){
+    return fetch('http://localhost:5000/books',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(book)
+    })
+    .then(res => res.json())
     .then(data => data);
 }
