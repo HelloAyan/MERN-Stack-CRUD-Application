b1d5feb50af2e618752a818904f5df32df051745
HelloAyan <18103373@iubat.edu>
2022-07-31 00:59:09 +0600

    Removed comments

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <div className="App">
-      {/*<CreateBook onCreate={addBook} />*/}
+      <CreateBook onCreate={addBook} />
       <TableBook books ={books}  />
     </div>
   );
