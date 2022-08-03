c41bda79a0be7252b3d604835b843e3ae3dfe55e
HelloAyan <18103373@iubat.edu>
2022-08-04 00:43:28 +0600

    installed react-router-dom & added class name

--- client/src/TableBook.js
+++ client/src/TableBook.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import "./App.css";
-import {Link} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 function TableBook(props){
     const {books, onDelete} = props;
 
@@ -17,7 +16,8 @@ function TableBook(props){
                         <tr key={book._id}>
                             <td className='show-title'>{book.title}</td>
                             <td className='show-description'>{book.description}</td>
-                            <td><button onClick={()=> onDelete(book._id)}>Delete</button></td>
+                            <td ><button class="updateCenter">Update</button></td>
+                            <td ><button className='deleteCenter' onClick={()=> onDelete(book._id)}>Delete</button></td>
                         </tr>
                     )
                 })
