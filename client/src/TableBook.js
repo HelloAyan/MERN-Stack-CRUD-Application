5a2d8d8e4c6c8ccd7bf7e9d8dbc9f4de81889b40
HelloAyan <18103373@iubat.edu>
2022-08-08 00:01:27 +0600

    Modify some code

--- client/src/TableBook.js
+++ client/src/TableBook.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import "./App.css";
 import { Link } from 'react-router-dom';
+
+
 function TableBook(props){
     const {books, onDelete} = props;
 
@@ -16,8 +18,8 @@ function TableBook(props){
                         <tr key={book._id}>
                             <td className='show-title'>{book.title}</td>
                             <td className='show-description'>{book.description}</td>
-                            <td ><button class="updateCenter">Update</button></td>
-                            <td ><button className='deleteCenter' onClick={()=> onDelete(book._id)}>Delete</button></td>
+                            <td className='inputCenter'><button class="updateCenter">Update</button></td>
+                            <td className='inputCenter'><button className='deleteCenter' onClick={()=> onDelete(book._id)}>Delete</button></td>
                         </tr>
                     )
                 })
